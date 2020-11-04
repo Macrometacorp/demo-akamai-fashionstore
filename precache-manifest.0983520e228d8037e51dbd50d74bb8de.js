4c2da60963628811e77bdeec583b89877c7f42f8
Abhishek <abhishek.lohani@macrometa.co>
2020-11-04 22:32:19 +0530

    Updates

---
+++ precache-manifest.0983520e228d8037e51dbd50d74bb8de.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d948bdf1ed1e8304fbd8ed9abd5fa7",
+    "url": "/tutorial-akamai-fashionstore/index.html"
+  },
+  {
+    "revision": "cf5e19823462a14a3954",
+    "url": "/tutorial-akamai-fashionstore/static/css/2.13ce12b6.chunk.css"
+  },
+  {
+    "revision": "f6ad295a0957a77a3b51",
+    "url": "/tutorial-akamai-fashionstore/static/css/main.6ce1fc4f.chunk.css"
+  },
+  {
+    "revision": "cf5e19823462a14a3954",
+    "url": "/tutorial-akamai-fashionstore/static/js/2.a49a2b46.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/tutorial-akamai-fashionstore/static/js/2.a49a2b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ad295a0957a77a3b51",
+    "url": "/tutorial-akamai-fashionstore/static/js/main.2792db50.chunk.js"
+  },
+  {
+    "revision": "6533a7d2fb4f3cf370f5",
+    "url": "/tutorial-akamai-fashionstore/static/js/runtime-main.8139e451.js"
+  },
+  {
+    "revision": "e99fad78c474a7a490717058994ba24c",
+    "url": "/tutorial-akamai-fashionstore/static/media/Erin.e99fad78.png"
+  },
+  {
+    "revision": "ba959c5132cca492ff9a7f410fc84671",
+    "url": "/tutorial-akamai-fashionstore/static/media/Jacob.ba959c51.png"
+  },
+  {
+    "revision": "f251214e4c9fe4dfc4e09bbfc6d7285e",
+    "url": "/tutorial-akamai-fashionstore/static/media/John.f251214e.png"
+  },
+  {
+    "revision": "f95a983ab2893a069c782d92ee6b9a70",
+    "url": "/tutorial-akamai-fashionstore/static/media/bestSellers.f95a983a.png"
+  },
+  {
+    "revision": "161c4a441a391c6bc596220c8db0a1e4",
+    "url": "/tutorial-akamai-fashionstore/static/media/burgers.161c4a44.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/tutorial-akamai-fashionstore/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/tutorial-akamai-fashionstore/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/tutorial-akamai-fashionstore/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/tutorial-akamai-fashionstore/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/tutorial-akamai-fashionstore/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "b9922f0a69ee0b31970ad28a82335a54",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-bags.b9922f0a.png"
+  },
+  {
+    "revision": "6f9f145077c2be3aaa0c6bb71448f42f",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-designers.6f9f1450.png"
+  },
+  {
+    "revision": "9f6ee2fca2e237a56c2c97d5c0501678",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-homewear.9f6ee2fc.jpg"
+  },
+  {
+    "revision": "32ac813bbcb5a79ffe01c9b1269da650",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-kids.32ac813b.png"
+  },
+  {
+    "revision": "aa13e79c4d79ab5c7da02f157d0adfdb",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-main.aa13e79c.png"
+  },
+  {
+    "revision": "a9581311748b574fff403c8006336a06",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-men.a9581311.png"
+  },
+  {
+    "revision": "98679286fa21e4fb224288dd9286f41f",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-sportswear.98679286.png"
+  },
+  {
+    "revision": "930907dee83e1383c4a35488096f892b",
+    "url": "/tutorial-akamai-fashionstore/static/media/hero-women.930907de.png"
+  },
+  {
+    "revision": "922459532b985f95bcbbf46428a6093f",
+    "url": "/tutorial-akamai-fashionstore/static/media/italian.92245953.png"
+  },
+  {
+    "revision": "86634adf1691832e085e24841bf1c183",
+    "url": "/tutorial-akamai-fashionstore/static/media/noodles.86634adf.png"
+  },
+  {
+    "revision": "22f42738e6496c5085dfa50133218292",
+    "url": "/tutorial-akamai-fashionstore/static/media/pancakes.22f42738.png"
+  },
+  {
+    "revision": "9fa03bfe92fc72fea97174e72e1e97d4",
+    "url": "/tutorial-akamai-fashionstore/static/media/pastOrders.9fa03bfe.png"
+  },
+  {
+    "revision": "203d8247e56a2f1c0fd7d52baf1b19c3",
+    "url": "/tutorial-akamai-fashionstore/static/media/pineapple.203d8247.png"
+  },
+  {
+    "revision": "d1b410f302ec5d3b7167aba139ee79e3",
+    "url": "/tutorial-akamai-fashionstore/static/media/screenshot.d1b410f3.png"
+  },
+  {
+    "revision": "0f89c06edb2fb41d5abcae96cf8281d5",
+    "url": "/tutorial-akamai-fashionstore/static/media/supportedCards.0f89c06e.png"
+  },
+  {
+    "revision": "c6f148dcb9b1d22b54c5177dc63a2996",
+    "url": "/tutorial-akamai-fashionstore/static/media/umami.c6f148dc.png"
+  },
+  {
+    "revision": "76f37cd9c73a4a657f53ac2c301dc74b",
+    "url": "/tutorial-akamai-fashionstore/static/media/yourpastorders.76f37cd9.png"
+  }
+]);
