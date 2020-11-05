66c09ae7253126c1abe1a92fb1da0a3e51a51a7d
Abhishek <abhishek.lohani@macrometa.co>
2020-11-05 15:32:51 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fashionstore.demo.macrometa.io/tutorial-akamai-fashionstore/precache-manifest.2aa30db59a643eefd4f33ad701201a56.js"
+  "./precache-manifest.fe2b5d03246ffac6f7dbc104dfba5edb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fashionstore.demo.macrometa.io/tutorial-akamai-fashionstore/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
